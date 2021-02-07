39e63df853693512001884c99b86d792d4620e1c
unknown <goncalves2541@gmail.com>
2021-02-07 15:10:00 +0100

    Change Object Data 4

--- src/pages/Home/Data.js
+++ src/pages/Home/Data.js
@@ -1,6 +1,7 @@
 import svg1 from '../../images/svg_online_discussion.svg';
 import svg2 from '../../images/event1.jpg';
 import svg3 from '../../images/svg_aircraft.svg';
+import svg4 from '../../images/svg_contact_us.svg';
 import animationData from '../../lottie/confetti-partyyy_bal.json';
 export const homeObj1 = {
   lightBg: false,
@@ -19,7 +20,9 @@ export const homeObj1 = {
   orientação profissional, ajudará você como desenvolvedor, 
   a crescer, ganhar visibilidade, e evoluir de forma 
   exponencial nesta carreira.`,
-  btnLabel: 'Get Started',
+  btnLabel: 'Quero fazer parte',
+  btnLink:
+    'https://gmail.us7.list-manage.com/subscribe?u=15c8a2eac045ad27e371caba3&id=d72a2d8a6a',
   start: '',
   img: svg1,
   lottie: false,
@@ -63,6 +66,8 @@ export const homeObj3 = {
   com outros membros da comunidade.
   `,
   btnLabel: 'Começar',
+  btnLink:
+    'https://gmail.us7.list-manage.com/subscribe?u=15c8a2eac045ad27e371caba3&id=d72a2d8a6a',
   start: '',
   img: svg3,
   lottie: true,
@@ -78,14 +83,17 @@ export const homeObj4 = {
   primary: false,
   bgImage: false,
   imgStart: 'start',
-  topLine: 'Marketing Agency',
-  headLine: 'Lead Generation Specialist for Online Business',
-  description: `We help business owners increase their revenu. 
-    Our team of unique specialist can help you acheive you
-    acheive your business goals.`,
-  btnLabel: 'Get Started',
+  // topLine: 'Entre em contacto',
+  headLine: 'Apoie a comunidade',
+  description: `Se tiver alguma sugestão, dúvida, crítica, doação ou quiser apoiar a comunidade
+    de alguma forma, por favor entre em contacto connosco.
+    Seu apoio valerá muito apena para o desenvolvimento de todos nós.  
+  `,
+  btnLabel: 'Entrar em contacto',
+  btnLink:
+    'https://us7.list-manage.com/contact-form?u=15c8a2eac045ad27e371caba3&form_id=e8500093c255d9b55df59f51d0db48fd',
   start: 'true',
-  img: require('../../images/svg-3.svg'),
+  img: svg4,
   lottie: false,
   alt: 'Image',
 };
