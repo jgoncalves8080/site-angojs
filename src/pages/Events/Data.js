a2ef152bf952332b46e2dbd13f8d8f36f4df8409
unknown <goncalves2541@gmail.com>
2021-02-07 13:51:12 +0100

    Config Data Events

---
+++ src/pages/Events/Data.js
@@ -0,0 +1,26 @@
+import svg1 from '../../images/svg_online_discussion.svg';
+import svg2 from '../../images/event1.jpg';
+import svg3 from '../../images/svg_aircraft.svg';
+import animationData from '../../lottie/confetti-partyyy_bal.json';
+
+export const homeObj1 = {
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  primary: false,
+  bgImage: false,
+  imgStart: 'start',
+  headLine: 'Abertura oficial da comunidade',
+  topLine: 'AngoJs Community',
+  description: `Neste evento, daremos abertura oficial à comunidade AngoJs, e 
+    vamos bater um papo com os nossos convidados especiais, que nos irão explicar
+    sobre a importância de participar em uma comunidade. 
+`,
+  btnLabel: 'Participar do Evento',
+  btnLink: 'https://www.youtube.com/watch?v=tzzEXZEEKng',
+  start: 'true',
+  img: svg2,
+  lottie: false,
+  alt: 'Image',
+};
