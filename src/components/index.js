89a271574cff0c781c1f9331eabd151a8117f81e
unknown <goncalves2541@gmail.com>
2021-02-07 13:41:37 +0100

    Index Components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar';
+export { default as InfoSection } from './InfoSection';
+export { default as Footer } from './Footer';
+export { default as Events } from './Events';
+export { default as About } from './About';
