1913f2113f20c3667498b1047b5d8fe6fb26491e
unknown <goncalves2541@gmail.com>
2021-02-07 13:41:12 +0100

    Data object About

---
+++ src/components/About/Data.js
@@ -0,0 +1,37 @@
+import openSource from '../../images/svg_open_source.svg';
+import conference from '../../images/svg_conference_speaker.svg';
+import training from '../../images/svg_instant_support.svg';
+export const dataAbout = [
+  {
+    id: 1,
+    img: openSource,
+    title: 'Projectos Open Source',
+    description: `
+    Identificaremos problemas do mundo real, e este projecto (desenvolvido pela comunidade) 
+    deverá resolvê-lo. Assim, além de aumentar sua experiência e conhecimento profissional,
+    estará também a contribuir para resolver um problema do mundo real.
+      `,
+  },
+  {
+    id: 2,
+    img: conference,
+    title: 'Palestras & WorkShops',
+    description: `
+    Teremos várias palestras e workshops falando sobre diferentes assuntos. Você terá a 
+    oportunidade de transmitir aquilo que sabe, à outros dev's, bem como 
+    também poderá aprender muito participando delass. Contacte-nos caso quiser transmitir
+    alguma coisa para a comunidade.
+      `,
+  },
+  {
+    id: 3,
+    img: training,
+    title: 'Treinamentos',
+    description: `
+    Nossa missão é levar você ao topo de sua carreira profissional.
+    Aqui na comunidade, você terá a oportunidade de receber treinamentos e orientação
+    profissional, tudo isto para lhe ajudar a atingir altos patamares. Fique ligado nas 
+    nossas redes sociais.
+      `,
+  },
+];
