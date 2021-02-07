f586b89553522a51a083c8629c9860be2fc4384a
unknown <goncalves2541@gmail.com>
2021-02-07 13:46:46 +0100

    Data for Footer

---
+++ src/components/Footer/DataLinksFooter.js
@@ -0,0 +1,75 @@
+export const FooterLinkData1 = [
+  {
+    title: 'How it works',
+    path: '/sign-up',
+  },
+  {
+    title: 'Testimonials',
+    path: '/',
+  },
+  {
+    title: 'Investors',
+    path: '/',
+  },
+  {
+    title: 'Term of Service',
+    path: '/',
+  },
+];
+
+export const FooterLinkData2 = [
+  {
+    title: 'How it works',
+    path: '/sign-up',
+  },
+  {
+    title: 'Testimonials',
+    path: '/',
+  },
+  {
+    title: 'Investors',
+    path: '/',
+  },
+  {
+    title: 'Term of Service',
+    path: '/',
+  },
+];
+
+export const FooterLinkData3 = [
+  {
+    title: 'How it works',
+    path: '/sign-up',
+  },
+  {
+    title: 'Testimonials',
+    path: '/',
+  },
+  {
+    title: 'Investors',
+    path: '/',
+  },
+  {
+    title: 'Term of Service',
+    path: '/',
+  },
+];
+
+export const FooterLinkData4 = [
+  {
+    title: 'How it works',
+    path: '/sign-up',
+  },
+  {
+    title: 'Testimonials',
+    path: '/',
+  },
+  {
+    title: 'Investors',
+    path: '/',
+  },
+  {
+    title: 'Term of Service',
+    path: '/',
+  },
+];
