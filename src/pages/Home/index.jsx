ac64dfcbe725bc8ae39e389fff441723e6d44250
unknown <goncalves2541@gmail.com>
2021-02-07 13:52:19 +0100

    Index Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { homeObj1, homeObj2, homeObj3, homeObj4 } from './Data';
+import { InfoSection, Events, About } from '../../components';
+
+function HomePage() {
+  return (
+    <>
+      <InfoSection {...homeObj1} />
+      <InfoSection {...homeObj2} />
+      <InfoSection {...homeObj3} />
+      <About />
+      <InfoSection {...homeObj4} />
+    </>
+  );
+}
+
+export default HomePage;
