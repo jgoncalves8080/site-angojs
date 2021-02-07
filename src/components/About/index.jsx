da0e18621da61da1d4d816778d15cdc10ea782bb
unknown <goncalves2541@gmail.com>
2021-02-07 15:08:21 +0100

    Add new feature on About

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import { Container, Card, CardImg, CardTiTle, CardBody } from './styles';
+import { Container, Card, CardImg, CardTiTle, CardBody, Title } from './styles';
 import { dataAbout } from './Data';
 
 function Sobre() {
   return (
-    <Container>
-      {dataAbout?.map((item) => (
-        <Card key={item?.id}>
-          <CardImg src={item?.img} alt={item?.title} />
-          <CardTiTle>{item?.title}</CardTiTle>
-          <CardBody>{item?.description}</CardBody>
-        </Card>
-      ))}
-    </Container>
+    <>
+      <Title>Nossas Activadades</Title>
+      <Container>
+        {dataAbout?.map((item) => (
+          <Card key={item?.id}>
+            <CardImg src={item?.img} alt={item?.title} />
+            <CardTiTle>{item?.title}</CardTiTle>
+            <CardBody>{item?.description}</CardBody>
+          </Card>
+        ))}
+      </Container>
+    </>
   );
 }
 
